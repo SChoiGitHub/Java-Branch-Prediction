6ad76fa667665c284790d7dd41658bb285561a8c
Sherman Choi <schoi@localhost.localdomain>
2017-06-12 12:25:15 -0500

    LoopHeuristic has been fixed? The heuristic now acknowledges that exits occasionally go back to the header.

--- LoopHeuristic.java
+++ LoopHeuristic.java
@@ -18,13 +18,33 @@ public class LoopHeuristic extends LoopFinder {
     protected void internalTransform (Body b, String phaseName, Map options){
 		super.internalTransform(b,phaseName,options);
 		//If we want the loops, we can call super.loops() to have it return a collection of loops
-		//this will print out all of the header units.
+		//this will print out all of the header units then print out all exits from that loop.
 		for(Loop l : super.loops()){
 			System.out.println("\tLoop Found, Header: " + l.getHead());
 			System.out.println("\t\tExits:");
 			for(Stmt s : l.getLoopExits()){
 				System.out.println("\t\t" + s);
-				System.out.println("\t\t\tPredict not taken to continue the loop.");
+				try{
+					//Is s an if statement?
+					IfStmt s_if = (IfStmt) s;
+					//If both are true, we check.
+					if(s_if.getTarget() != l.getHead()){
+						//The header is not the destination of the goto if it is taken. 
+						System.out.println("\t\t\tPredict not taken to continue the loop.");
+					}else{
+						System.out.println("\t\t\tPredict taken to continue the loop.");
+					}
+				}catch(Exception e1){
+					try{
+						//Is this a switch statment?
+						SwitchStmt s_sw = (SwitchStmt) s;
+						System.out.println("Switch Heuristics are not accounted for yet.");
+					}catch(Exception e2){
+						//Okay, I don't know how to resolve this yet.
+						System.out.println("\t\t\tThere was an unknown Heuristic Failure");				
+					}						
+				}
+				
 			}
 		}
     }
